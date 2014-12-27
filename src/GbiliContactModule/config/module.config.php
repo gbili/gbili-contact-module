194775fd25e67181178da7ee0a94455af9f31be7
Guillermo Devi <g@gbili.com>
2014-12-27 18:37:29 +0100

    symlink for contact form partial

--- src/GbiliContactModule/config/module.config.php
+++ src/GbiliContactModule/config/module.config.php
@@ -2,5 +2,8 @@
 namespace GbiliContactModule;
 
 return array(
+    'gbili_symlink_module' => array(
+        'gbili_contact_module_contact_form_phtml' => __DIR__ . '/../view/partial/contact_form.phtml', 
+    ),
     'controller_plugins'       => include __DIR__ . '/controller_plugins.config.php',
 );
