a759cb0a1eb76b57bcd7b6cc57561cc639e12019
Guillermo Devi <g@gbili.com>
2014-12-27 18:51:38 +0100

    doctrine config

--- src/GbiliContactModule/config/module.config.php
+++ src/GbiliContactModule/config/module.config.php
@@ -5,5 +5,6 @@ return array(
     'gbili_symlink_module' => array(
         'gbili_contact_module_contact_form_phtml' => __DIR__ . '/../view/partial/contact_form.phtml', 
     ),
-    'controller_plugins'       => include __DIR__ . '/controller_plugins.config.php',
+    'controller_plugins' => include __DIR__ . '/controller_plugins.config.php',
+    'doctrine' => include __DIR__ . '/doctrine.config.php',
 );
