e5a20bc7f1d8ac64ab8da34dfab97170b9fbbd29
Guillermo Devi <g@gbili.com>
2015-01-06 16:38:23 +0100

    Phone contact

---
+++ src/GbiliContactModule/src/GbiliContactModule/Entity/PhoneMessage.php
@@ -0,0 +1,178 @@
+<?php
+namespace GbiliContactModule\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity 
+ * @ORM\Table(name="gbili_contact_module_phone_message")
+ */
+class PhoneMessage 
+{
+    /**
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="firstname", type="string", length=64)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(name="lastname", type="string", length=64)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(name="phone", type="string", length=64)
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(name="company", type="string", length=64)
+     */
+    private $company;
+
+    /**
+     * @ORM\Column(name="service", type="string", length=64)
+     */
+    private $service;
+
+    /**
+     * @ORM\Column(name="locale", type="string", length=6)
+     */
+    private $locale;
+
+    /**
+     * @ORM\Column(name="body", type="text", length=64)
+     */
+    private $body;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime", nullable=false)
+     */
+    private $date;
+
+    public function __construct()
+    {
+    }
+
+    public function hasId()
+    {
+        return null !== $this->id;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+    }
+
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+    }
+
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    public function setCompany($company)
+    {
+        $this->company = $company;
+    }
+
+    public function getCompany()
+    {
+        return $this->company;
+    }
+
+    public function setService($service)
+    {
+        $this->service = $service;
+    }
+
+    public function getService()
+    {
+        return $this->service;
+    }
+
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+    }
+
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function setDate(\DateTime $time)
+    {
+        $this->date = $time;
+    }
+
+    /**
+     * Get Created Date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+}
---
+++ src/GbiliContactModule/src/GbiliContactModule/Form/Fieldset/PhoneMessage.php
@@ -0,0 +1,247 @@
+<?php
+namespace GbiliContactModule\Form\Fieldset;
+
+class PhoneMessage extends \Zend\Form\Fieldset 
+implements \Zend\InputFilter\InputFilterProviderInterface
+{
+    public function __construct($sm)
+    {
+        parent::__construct('phone-message');
+
+        $objectManager = $sm->get('Doctrine\ORM\EntityManager');
+
+        $this->setHydrator(new \DoctrineModule\Stdlib\Hydrator\DoctrineObject($objectManager))
+             ->setObject(new \GbiliContactModule\Entity\DefaultMessage());
+        
+        $this->add(array(
+            'name' => 'id',
+            'type'  => 'Zend\Form\Element\Hidden',
+        ));
+
+        $this->add(array(
+            'name' => 'firstname',
+            'type'  => 'Zend\Form\Element\Text',
+            'options' => array(
+                'label' => 'Firstname'
+            ),
+            'attributes' => array(
+                'class' => 'form-control',
+                'placeholder' => 'Johnny',
+            )
+        ));
+
+        $this->add(array(
+            'name' => 'lastname',
+            'type'  => 'Zend\Form\Element\Text',
+            'options' => array(
+                'label' => 'Lastname'
+            ),
+            'attributes' => array(
+                'class' => 'form-control',
+                'placeholder' => 'De Vito',
+            )
+        ));
+
+        $this->add(array(
+            'name' => 'phone',
+            'type'  => 'Zend\Form\Element\Text',
+            'options' => array(
+                'label' => 'Phone'
+            ),
+            'attributes' => array(
+                'class' => 'form-control',
+                'placeholder' => '41 22 000 00 00',
+            )
+        ));
+
+        $this->add(array(
+            'name' => 'email',
+            'type'  => 'Zend\Form\Element\Email',
+            'options' => array(
+                'label' => 'Email'
+            ),
+            'attributes' => array(
+                'class' => 'form-control',
+                'placeholder' => 'johnny.devito@example.com',
+            )
+        ));
+
+        $this->add(array(
+            'name' => 'company',
+            'type'  => 'Zend\Form\Element\Text',
+            'options' => array(
+                'label' => 'Where do you work?'
+            ),
+            'attributes' => array(
+                'class' => 'form-control',
+                'placeholder' => 'Company name',
+            )
+        ));
+        
+        $this->add(array(
+            'name' => 'service',
+            'type'  => 'Zend\Form\Element\Select',
+            'options' => array(
+                'label' => 'How can we help you?',
+                'empty_option' => '---',
+                'value_options' => array(
+                    'website' => 'Build my Website',
+                    'ads' => 'Online Advertising',
+                    'automate' => 'Automate',
+                    'other' => 'Something else',
+                ),
+            ),
+            'attributes' => array(
+                'class' => 'form-control',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'body',
+            'type'  => 'Zend\Form\Element\Textarea',
+            'options' => array(
+                'label' => 'Message',
+            ),
+            'attributes' => array(
+                'class' => 'form-control',
+                'rows' => '8',
+                'placeholder' => 'Your message here...',
+            )
+        ));
+    }
+
+    public function getInputFilterSpecification()
+    {
+        return array(
+            'id' => array(
+                'required' => false,
+                'filters'  => array(
+                    array('name' => 'Int'),
+                ),
+            ),
+
+            'firstname' => array(
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 255,
+                        ),
+                    ),
+                ),
+            ),
+
+            'lastname' => array(
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 255,
+                        ),
+                    ),
+                ),
+            ),
+
+            'phone' => array(
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'PhoneNumber',
+                    ),
+                ),
+            ),
+
+            'email' => array(
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'EmailAddress',
+                    ),
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 255,
+                        ),
+                    ),
+                ),
+            ),
+
+            'company' => array(
+                'required' => false,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 255,
+                        ),
+                    ),
+                ),
+            ),
+
+            'service' => array(
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 255,
+                        ),
+                    ),
+                ),
+            ),
+
+            'body' => array(
+                'required' => false,
+                'filters'  => array(
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 700,
+                        ),
+                    ),
+                ),
+            ),
+        );
+    }
+}
