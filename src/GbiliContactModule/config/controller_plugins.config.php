b4aaa97824173696acfe6817b7ac997bbb3e4d10
Guillermo Devi <g@gbili.com>
2015-05-05 11:09:52 +0200

    view messages plugin

--- src/GbiliContactModule/config/controller_plugins.config.php
+++ src/GbiliContactModule/config/controller_plugins.config.php
@@ -4,5 +4,6 @@ namespace GbiliContactModule;
 return array(
     'invokables' => array(
         'gbiliContact' => __NAMESPACE__ . '\Controller\Plugin\Contact',
+        'gbiliContactMessages' => __NAMESPACE__ . '\Controller\Plugin\Messages',
     ),
 );
---
+++ src/GbiliContactModule/src/GbiliContactModule/Controller/Plugin/Messages.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+namespace GbiliContactModule\Controller\Plugin;
+
+/**
+ *
+ */
+class Messages extends \Zend\Mvc\Controller\Plugin\AbstractPlugin
+{
+    /**
+     * Contact Form Action 
+     * @return mixed
+     */
+    public function __invoke(array $routeSuccessParams)
+    {
+        $controller = $this->getController();
+
+        $em      = $controller->em();
+        $sm      = $controller->getServiceLocator();
+        $request = $controller->getRequest();
+        $locale  = $controller->locale();
+
+        $config = $sm->get('Config');
+        $messageEntityClass = (isset($config['gbili_contact_module']['message_entity_class']))
+            ? $config['gbili_contact_module']['message_entity_class']
+            : '\GbiliContactModule\Entity\DefaultMessage';
+
+        return new \Zend\View\Model([
+            'messages' => $em->getRepository($messageEntityClass)->findAll(),
+        ]);
+    }
+}
